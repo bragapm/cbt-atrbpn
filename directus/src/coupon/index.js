1a20e7ac8a19d529496bdd3feaa2685e705dd06d
muharyman <muharymank@gmail.com>
2024-10-21 15:57:20 +0700

    fix:register coupon

--- directus/src/coupon/index.js
+++ directus/src/coupon/index.js
@@ -3,10 +3,9 @@ import { v4 as uuidv4 } from "uuid";
 export default (router, { services, exceptions, getSchema }) => {
   const { UsersService, ItemsService, AuthenticationService } = services;
 
-  router.post("/register", async (req, res, next) => {
+  router.post("/register", async (req, res) => {
     try {
       const { coupon_code } = req.body;
-
       // Generate email dan password secara otomatis
       const uniqueId = uuidv4();
       const user_email = `user_${uniqueId}@example.com`;
@@ -28,47 +27,42 @@ export default (router, { services, exceptions, getSchema }) => {
 
       const now = new Date();
 
-      const a = await couponsService.createOne({
+      await couponsService.createOne({
         code: coupon_code,
-        user_id: newUser, // Menggunakan newUser.id
+        user_id: newUser,
         status: "Published",
         created_at: now,
         updated_at: now,
         last_login_at: now,
       });
-      
-      // Autentikasi untuk mendapatkan access_token dan refresh_token
+
       const authService = new AuthenticationService({
         accountability: null,
         schema: await getSchema(),
       });
 
-      console.log({ authService });
-      console.log('Attempting login with:', { email: user_email, password: user_password });
-      const authData = await authService.login(undefined,{
+      const authData = await authService.login(undefined, {
         email: user_email,
         password: user_password,
       });
 
-      console.log("authdata",authData);
-
-      // Kembalikan informasi yang dibutuhkan
       res.json({
-        email: user_email,
-        password: user_password,
-        last_login_at: newUser.last_login_at,
-        access_token: authData.accessToken,
-        refresh_token: authData.refreshToken,
+        status: "succes",
+        data: {
+          email: user_email,
+          password: user_password,
+          last_login_at: newUser.last_login_at,
+          access_token: authData.accessToken,
+          refresh_token: authData.refreshToken,
+        },
       });
-
     } catch (error) {
-      // Gunakan generic error handler jika ServiceUnavailableException tidak tersedia
-      console.error("Error creating user or coupon:", error);
-      next(new Error(`Error creating user or coupon: ${error.message}`));
+      console.error(error);
+      res.json({
+        status: "error",
+        error: new Error(error).message,
+      });
+      // next(new Error(`Error creating user or coupon: ${error.message}`));
     }
   });
-
-  router.get("/", async (req, res) =>
-    res.json({ status: "success", data: "Hello" })
-  );
 };
