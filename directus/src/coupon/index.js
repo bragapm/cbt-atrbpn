56db85c083cdda587d4630a7123ffcfa8ead6786
muharyman <muharymank@gmail.com>
2024-10-21 13:39:25 +0700

    feat:coupon

---
+++ directus/src/coupon/index.js
@@ -0,0 +1,73 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default (router, { services, exceptions, getSchema }) => {
+  const { UsersService, ItemsService, AuthenticationService } = services;
+
+  router.post("/register", async (req, res, next) => {
+    try {
+      const { coupon_code } = req.body;
+
+      // Generate email dan password secara otomatis
+      const uniqueId = uuidv4();
+      const user_email = `user_${uniqueId}@example.com`;
+      const user_password = "CBTATRBPN2024"; // Generate password acak
+
+      // Buat akun baru untuk pengguna
+      const usersService = new UsersService({ schema: await getSchema() });
+
+      const newUser = await usersService.createOne({
+        email: user_email,
+        password: user_password,
+        status: "active",
+      });
+
+      // Buat item kupon baru
+      const couponsService = new ItemsService("coupon", {
+        schema: await getSchema(),
+      });
+
+      const now = new Date();
+
+      const a = await couponsService.createOne({
+        code: coupon_code,
+        user_id: newUser, // Menggunakan newUser.id
+        status: "Published",
+        created_at: now,
+        updated_at: now,
+        last_login_at: now,
+      });
+
+      // Autentikasi untuk mendapatkan access_token dan refresh_token
+      const authService = new AuthenticationService({
+        accountability: null,
+        schema: await getSchema(),
+      });
+
+      console.log({ authService });
+
+      const authData = await authService.login({
+        email: user_email,
+        password: user_password,
+      });
+
+      console.log("www");
+
+      // Kembalikan informasi yang dibutuhkan
+      res.json({
+        email: user_email,
+        password: user_password,
+        last_login_at: newUser.last_login_at,
+        access_token: authData.access_token,
+        refresh_token: authData.refresh_token,
+      });
+    } catch (error) {
+      // Gunakan generic error handler jika ServiceUnavailableException tidak tersedia
+      console.error("Error creating user or coupon:", error);
+      next(new Error(`Error creating user or coupon: ${error.message}`));
+    }
+  });
+
+  router.get("/", async (req, res) =>
+    res.json({ status: "success", data: "Hello" })
+  );
+};
