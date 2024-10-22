36407d95103878526050eb8d5837e65997744955
ridwani <ridwanibrahim347@gmail.com>
2024-10-22 15:53:36 +0700

    - bg login - jawaban - navigasi soal

--- client/src/layouts/AuthLayout.tsx
+++ client/src/layouts/AuthLayout.tsx
@@ -4,7 +4,10 @@ import MainLayout from "./MainLayout";
 const AuthLayout = () => {
   return (
     <MainLayout variant="auth">
-      <div className="bg-gray-200 w-full h-[85vh] rounded-lg p-8">
+      <div
+        className="bg-gray-200 w-full h-[85vh] rounded-lg p-8"
+        style={{ backgroundImage: `url(/images/bg-login-1.png)` }}
+      >
         <Outlet />
       </div>
     </MainLayout>
