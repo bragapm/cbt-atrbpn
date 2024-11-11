01c8ebce680217e22249f74ff5703916ab5d213c
Ahmad Ansorudin <65240136+ansorudin@users.noreply.github.com>
2024-11-11 08:17:42 +0700

    feat: add toast when create peserta failed (#31)
    * feat: update several page
    
    * fix: create and edit peserta
    
    * feat: add toast when create peserta failed

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,11 +2,14 @@ import { RouterProvider } from "react-router-dom";
 import ErrorFallback from "./components/ErrorFallback";
 import { ErrorBoundary } from "react-error-boundary";
 import router from "./routes/routes";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App: React.FC = () => {
   return (
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <RouterProvider router={router} />
+      <ToastContainer />
     </ErrorBoundary>
   );
 };
