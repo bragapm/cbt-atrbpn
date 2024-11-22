11eec14fe5bb5518f86619b51b3914a3364c5125
RifqiM <rifqi546@gmail.com>
2024-11-22 14:22:49 +0700

    fix join

--- directus/src/jumlah-peserta/index.js
+++ directus/src/jumlah-peserta/index.js
@@ -7,25 +7,27 @@ export default function registerEndpoint(router, { database, logger }) {
             const offset = (page - 1) * limit;
 
             const query = `
-                SELECT 
-                    session AS session_id,
-                    st.name  AS "nama",
-                    DATE(st.start_time) as "tangagl_ujian",
-                    st.start_time::TIME AS "mulai_ujian",
-                    st.end_time::TIME as "selesai_ujian",
-                    COUNT(*) AS "jumlah_peserta"
-                FROM 
-                    user_session_test ust
-                JOIN 
-                    session_test st 
-                ON 
-                    st.id = ust."session" 
-                GROUP BY 
-                    session, st.name, st.start_time, st.end_time 
-                ORDER BY 
-                    session ASC
-                LIMIT ? OFFSET ?;
-            `;
+            SELECT 
+                st.id AS session_id,
+                st.name AS "nama",
+                DATE(st.start_time) AS "tanggal_ujian",
+                st.start_time::TIME AS "mulai_ujian",
+                st.end_time::TIME AS "selesai_ujian",
+                COUNT(ust.session) AS "jumlah_peserta"
+            FROM 
+                session_test st
+            LEFT JOIN 
+                user_session_test ust 
+            ON 
+                st.id = ust.session 
+            GROUP BY 
+                st.id, st.name, st.start_time, st.end_time 
+            ORDER BY 
+                st.id ASC
+            LIMIT ? OFFSET ?;
+        `;
+        
+        
 
             const result = await database.raw(query, [limit, offset]);
             const rows = result.rows || [];
