6d86b115c1802ddb080ee6f6362993cce2e81ecf
RifqiM <rifqi546@gmail.com>
2024-11-27 13:57:43 +0700

    fix jawaban benar

--- directus/src/jawaban-peserta/index.js
+++ directus/src/jawaban-peserta/index.js
@@ -1,3 +1,4 @@
+//endpoint jawaban peserta
 export default function registerEndpoint(router, { database, logger }) {
     router.get('/', async (req, res) => {
       try {
@@ -11,38 +12,38 @@ export default function registerEndpoint(router, { database, logger }) {
         }
   
         const query = `
-          WITH assigned_problems AS (
+          
+        WITH assigned_problems AS (
             SELECT json_array_elements_text(problems)::INTEGER AS problem_id
             FROM user_session_test
-            WHERE id = ?
-          ),
-          answered_problems AS (
+            WHERE id = 10704
+            ),
+        answered_problems AS (
             SELECT problem, 
-                   score::NUMERIC, 
-                   correct_score::NUMERIC, 
-                   score_category::INTEGER
+                    score::NUMERIC, 
+                    score_category::INTEGER
             FROM user_test
-            WHERE user_session_id = ?
-          ),
-          unanswered_problems AS (
+            WHERE user_session_id = 10704
+            ),
+        unanswered_problems AS (
             SELECT ap.problem_id
             FROM assigned_problems ap
             LEFT JOIN answered_problems ans ON ap.problem_id = ans.problem
             WHERE ans.problem IS NULL
-          )
-          SELECT 
+            )
+        SELECT 
             qb.id AS "id",
             qb.question AS "soal_pertanyaan",
             COALESCE(ans.score_category, 0::INTEGER) AS "Jawaban",
             ks.nama_kategori AS "Kategori",
             COALESCE(ans.score, 0::NUMERIC) AS "Skor",
-            COALESCE(ans.correct_score, 0::NUMERIC) AS "nilai_jawaban_benar",
-            ms.materi as materi
-          FROM assigned_problems ap
-          LEFT JOIN answered_problems ans ON ap.problem_id = ans.problem
-          LEFT JOIN questions_bank qb ON ap.problem_id = qb.id
-          LEFT JOIN kategori_soal ks ON qb.kategori_id = ks.id
-          LEFT JOIN materi_soal ms ON qb.materi_id = ms.id;
+            COALESCE(ks.bobot_benar, 0::NUMERIC) AS "nilai_jawaban_benar",
+            ms.materi AS "materi"
+        FROM assigned_problems ap
+        LEFT JOIN answered_problems ans ON ap.problem_id = ans.problem
+        LEFT JOIN questions_bank qb ON ap.problem_id = qb.id
+        LEFT JOIN kategori_soal ks ON qb.kategori_id = ks.id
+        LEFT JOIN materi_soal ms ON qb.materi_id = ms.id;
         `;
   
         const result = await database.raw(query, [user_session_id, user_session_id]);
